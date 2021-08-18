72ade3c51b56a9715c7d509b62efec6a35cfe6c8
Philip <philip.mathew1998@gmail.com>
2021-08-18 22:17:08 +0530

    ENVIRON

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import discord
 import os
 from datetime import datetime
 from PyDictionary import PyDictionary as dictionary
+from discord import channel
 from basic_commands import run_bot
+from discord.ext import commands
 
 client = discord.Client()
 
@@ -10,6 +12,8 @@ client = discord.Client()
 async def on_ready():
   print("We have logged in as {0.user}".format(client))
 
+
 run_bot(client)
 
-client.run("ODc3NDU5NzcyNjE2NjIyMTMw.YRy8GA.wkgrn_-dV-zQgRXGkoWJy02dEWE")
+client.run(os.environ.get("TOKEN"))
+#client.run("ODc3NDU5NzcyNjE2NjIyMTMw.YRy8GA.wkgrn_-dV-zQgRXGkoWJy02dEWE")
