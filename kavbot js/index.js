39c48620fa54f7158f825abfd9262b0de2d0af47
natsudragneel5 <alroy.fern7@gmail.com>
2021-08-19 13:25:50 +0530

    on message modified

--- kavbot js/index.js
+++ kavbot js/index.js
@@ -1,16 +1,15 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, Message } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", gotMessage);
-
-function gotMessage(msg) {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+client.on("message", async (Message) => {
+  if (Message.content === "hi") {
+    console.log("Got command");
+    await Message.reply("Pong!");
   }
-}
+});
 
 client.login("ODc3NDU5NzcyNjE2NjIyMTMw.YRy8GA.wkgrn_-dV-zQgRXGkoWJy02dEWE");
