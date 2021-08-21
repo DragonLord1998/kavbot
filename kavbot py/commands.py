9faf2f2d60790cded20057a06a4cfcdfe7d1fe39
Philip <philip.mathew1998@gmail.com>
2021-08-21 15:58:48 +0530

    Update commands.py

--- kavbot py/commands.py
+++ kavbot py/commands.py
@@ -4,7 +4,9 @@ def commands_kav():
                     'what is <term>':'  Returns Definitions of term from the dictionary',
                     'hi':'  Returns Hi Ho',
                     'music':'  Try it',
-                    'time':'  Returns current time'
+                    'time':'  Returns current time',
+                    'weater <city_name>' :'Returns current weather at that place',
+                    'meaning' : 'Returns the meaning of the word from an online dictionary'
                    }
     pre_commands = json.dumps(pre_commands, indent=0)
     return pre_commands
