9dc427dcf72e9fad6445f4e6b5bc2f62296e9709
Philip <philip.mathew1998@gmail.com>
2021-08-21 13:04:53 +0530

    Update dictionaries.py

--- kavbot py/dictionaries.py
+++ kavbot py/dictionaries.py
@@ -5,5 +5,5 @@ def dict(word):
     res = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
     if res.status_code == 200:
         val= res.json()
-        return((val[0]['meanings'][1]['definitions'][0]['definition']))
+        return((val[0]['meanings'][0]['definitions'][0]['definition']))
 
