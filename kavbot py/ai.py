4f437fe42a2d1755b06469175eedd02113db126c
Philip <philip.mathew1998@gmail.com>
2021-08-26 11:02:43 +0530

    changes

--- kavbot py/ai.py
+++ kavbot py/ai.py
@@ -3,7 +3,13 @@ import openai as ai
 def smartai(ques):
     def chat(question,chat_log = None) -> str:
         if(chat_log == None):
-            chat_log = start_chat_log
+            start_chat_log = """Human: Hello, I am Human.
+                             AI: Hello, human I am KavBot.
+                             Human: How are you?
+                             AI: I am fine, thanks for asking. 
+                             """ 
+            start_chat_log = start_chat_log + ques
+            chat_log = start_chat_log 
         prompt = f"{chat_log}Human: {question}\nAI:"
         response = completion.create(prompt = prompt, engine =  "davinci", temperature = 0.85,top_p=1, frequency_penalty=0, 
         presence_penalty=0.7, best_of=1,max_tokens=100,stop = "\nHuman: ")
@@ -15,11 +21,7 @@ def smartai(ques):
     
     completion = ai.Completion()
 
-    start_chat_log = """Human: Hello, I am Human.
-    AI: Hello, human I am KavBot.
-    Human: How are you?
-    AI: I am fine, thanks for asking. 
-    """
+    
 
         #train = False
         
